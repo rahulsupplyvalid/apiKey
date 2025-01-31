3ca431d40f726f82bddff5b19b7e665b425f404e
rahulsupplyvalid <rkumar@supplyvalid.com>
2025-01-31 16:56:54 +0530

    code merge

---
+++ Migrations/20250131103912_update201.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace masterapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class update201 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
